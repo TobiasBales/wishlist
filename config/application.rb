44fbeda7de1f4b14af75236d71849a465782cdff
Tobias Bales <hi@tobiasbales.com>
2023-01-22 12:33:02 +0100

    feat: use uuids as keys for scaffolds

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +18,8 @@ module Wishlist
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
