6785bda36745138e9000c700a1ba04877a327ef9
Tobias Bales <hi@tobiasbales.com>
2023-01-22 20:30:16 +0100

    feat: show complate item images

--- app/components/bootstrap/card.rb
+++ app/components/bootstrap/card.rb
@@ -9,16 +9,20 @@ module Bootstrap
 
     sig do
       params(title: T.nilable(String), subtitle: T.nilable(String), text: T.nilable(String),
-             image: T.nilable(Image)).void
+             image: T.nilable(Image), ratio: String, image_fill: String).void
     end
-    def initialize(title: nil, subtitle: nil, text: nil, image: nil)
+    # rubocop:disable Metrics/ParameterLists
+    def initialize(title: nil, subtitle: nil, text: nil, image: nil, ratio: "16x9", image_fill: "cover")
       super
 
       @title = title
       @subtitle = subtitle
       @text = text
       @image = image
+      @ratio = ratio
+      @image_fill = image_fill
     end
+    # rubocop:enable Metrics/ParameterLists
 
     renders_one :body
 
@@ -33,6 +37,12 @@ module Bootstrap
 
     sig { returns(T.nilable(String)) }
     attr_reader :text
+
+    sig { returns(String) }
+    attr_reader :ratio
+
+    sig { returns(T.nilable(String)) }
+    attr_reader :image_fill
 
     sig { returns(String) }
     def classes
